1c51ba2adabd385961d625b3395bf402006c6ec0
KK964 <morrkade05@gmail.com>
2023-06-11 11:18:43 -0500

    Fixed piping issue

--- src/audio/recordAudio.ts
+++ src/audio/recordAudio.ts
@@ -2,7 +2,7 @@ import { EndBehaviorType, VoiceReceiver, getVoiceConnection } from '@discordjs/v
 import type { Guild, User, VoiceBasedChannel } from 'discord.js';
 
 import { existsSync, mkdirSync, createWriteStream } from 'node:fs';
-import { pipeline, Transform } from 'node:stream';
+import { pipeline } from 'node:stream';
 
 import * as prism from 'prism-media';
 import { canRecord } from './usersToRecord';
@@ -10,11 +10,6 @@ import { canRecord } from './usersToRecord';
 import CONSTANTS from '../utils/constants';
 
 let startTime: number | undefined;
-
-const ENDING_BYTES = [
-  0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe, 0xf8,
-  0xff, 0xfe, 0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe, 0xf8, 0xff, 0xfe,
-];
 
 export async function startRecordingAudio(guild: Guild) {
   const connection = getVoiceConnection(guild.id);
@@ -31,22 +26,14 @@ export async function startRecordingAudio(guild: Guild) {
   return true;
 }
 
-export async function stopRecordingAudio(guild: Guild) {
-  const connection = getVoiceConnection(guild.id);
-  if (!connection) return;
-  const receiver = connection.receiver;
-  receiver.speaking.removeAllListeners('start');
-  receiver.subscriptions.forEach((subscription) => {
-    subscription.destroy();
-  });
-}
-
 function ensureRecordingAllUsers(channel: VoiceBasedChannel) {
   const connection = getVoiceConnection(channel.guild.id);
   if (!connection) return;
   const receiver = connection.receiver;
   channel.members.forEach((member) => {
     try {
+      if (member.voice.mute) return;
+      if (member.id === channel.guild.members.me?.id) return;
       if (!canRecord(member.user)) return;
       createListeningStream(receiver, member.id, member.user);
     } catch (_) {}
@@ -95,34 +82,7 @@ function createListeningStream(receiver: VoiceReceiver, userId: string, user?: U
 
   const out = createWriteStream(filename);
 
-  const prematureCloseFix = new Transform({
-    transform(chunk, _encoding, callback) {
-      if (chunk.length < 30) {
-        callback(null, chunk);
-        return;
-      }
-
-      // Ensure the last 30 bytes of the stream are ENDING_BYTES
-      const lastBytes = chunk.slice(chunk.length - 30);
-      let allEndingBytes = true;
-      for (let i = 0; i < 30; i++) {
-        if (lastBytes[i] == ENDING_BYTES[i]) continue;
-        allEndingBytes = false;
-        break;
-      }
-      if (allEndingBytes) {
-        callback(null, chunk);
-        return;
-      }
-
-      console.warn(`Fixing premature close of stream (${chunk.length} bytes)`);
-
-      // Append the ending bytes to the stream
-      callback(null, Buffer.concat([chunk, Buffer.from(ENDING_BYTES)]));
-    },
-  });
-
-  pipeline(opusStream, prematureCloseFix, oggStream, out, (err) => {
+  pipeline(opusStream, oggStream, out, (err) => {
     if (!err) return;
     console.warn(`Error recording file ${filename} - ${err.message}`);
   });
